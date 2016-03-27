50bbed9481d6081879711c40ece9de5ebadf6adc
anajacimovic1995 <anajac95@gmail.com>
2016-03-28 01:53:46 +0200

    Methode addNewRate is implemented diferently on master branch than it is implemented on implementacijainterfejsa branch.

--- CurrencyExchange/src/currencyExchangeInterface/CurrencyExchange.java
+++ CurrencyExchange/src/currencyExchangeInterface/CurrencyExchange.java
@@ -1,29 +1,52 @@
 package currencyExchangeInterface;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import currencies.Currency;
 import currencies.rate.Rate;
 
 public class CurrencyExchange implements currencyInterface {
 
+	private LinkedList<Rate> rates;
+	
+	public CurrencyExchange(LinkedList<Rate> rates) {
+		this.rates=rates;
+	}
+	
 	@Override
 	public void addNewRate(Currency currency, GregorianCalendar date, double s, double m, double p) {
-		// TODO Auto-generated method stub
-
+		
+		Rate r = new Rate();
+		r.setDate(date);
+		r.setMedium(m);
+		r.setPurchasable(p);
+		r.setSales(s);
+		
+		if(rates.contains(r))
+			throw new RuntimeException("It is already exist!");
+		rates.add(r);
 	}
 
 	@Override
 	public void deleteRate(Currency currency, GregorianCalendar date) {
-		// TODO Auto-generated method stub
+		for(int i=0; i<rates.size(); i++){
+			if(rates.get(i).getDate().equals(date)){
+				rates.remove(i);
+			}
+		}
 
 	}
 
 	@Override
 	public Rate restoreRate(Currency currency, GregorianCalendar date) {
+		for(int i=0; i<rates.size(); i++){
+			if(rates.get(i).getDate().equals(date)){
+				return rates.get(i);
+			}
+		}
 		
 		return null;
-		
 	}
 
 }
