174a8b6869c18fdfedf7773b3fdcb648eb2c3e59
anajacimovic1995 <anajac95@gmail.com>
2016-03-28 00:56:11 +0200

    CurrencyExchange is new class in pachage currencyExchangeInterface. This class implements interface surrencyInterface. Methodes are empty.

---
+++ CurrencyExchange/src/currencyExchangeInterface/CurrencyExchange.java
@@ -0,0 +1,27 @@
+package currencyExchangeInterface;
+
+import java.util.GregorianCalendar;
+
+import currencies.Currency;
+
+public class CurrencyExchange implements currencyInterface {
+
+	@Override
+	public void addNewRate(Currency currency, GregorianCalendar date, double s, double m, double p) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteRate(Currency currency, GregorianCalendar date) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void restoreRate(Currency currency, GregorianCalendar date) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
