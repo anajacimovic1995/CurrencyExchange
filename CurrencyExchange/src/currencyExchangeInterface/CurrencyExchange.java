d9b8bca39f65e7824232ba87b6f76d020f4d9431
anajacimovic1995 <anajac95@gmail.com>
2016-03-28 01:04:37 +0200

    Constructor for CurrencyExchange is made. Parameter for it is LinkedList of currencies.

--- CurrencyExchange/src/currencyExchangeInterface/CurrencyExchange.java
+++ CurrencyExchange/src/currencyExchangeInterface/CurrencyExchange.java
@@ -1,11 +1,18 @@
 package currencyExchangeInterface;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import currencies.Currency;
 
 public class CurrencyExchange implements currencyInterface {
-
+	
+	private LinkedList<Currency> currencies;
+	
+	public CurrencyExchange(LinkedList<Currency> currencies) {
+		this.currencies = currencies;
+	}
+	
 	@Override
 	public void addNewRate(Currency currency, GregorianCalendar date, double s, double m, double p) {
 		// TODO Auto-generated method stub
