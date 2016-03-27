dbacd045a2b4d02cd00c81bc9336d837aa65614e
anajacimovic1995 <anajac95@gmail.com>
2016-03-28 00:05:19 +0200

    Methodes equals, hashCode i toString are made for classes Currency and Rate. toString methode of class Currency gives information about name and abbreviated name. toString methode of class Rate gives information about date and type of rate. Methode equals sorts class Currency by abbreviated name and class Rate by date.

--- CurrencyExchange/src/currencies/Currency.java
+++ CurrencyExchange/src/currencies/Currency.java
@@ -17,6 +17,34 @@ public class Currency {
 	public void setAbbreviatedName(String abbreviatedName) {
 		this.abbreviatedName = abbreviatedName;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((abbreviatedName == null) ? 0 : abbreviatedName.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Currency other = (Currency) obj;
+		if (abbreviatedName == null) {
+			if (other.abbreviatedName != null)
+				return false;
+		} else if (!abbreviatedName.equals(other.abbreviatedName))
+				return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Currency [Name=" + name + ", AbbreviatedName=" + abbreviatedName + "]";
+	}
 	
 	
 	
--- CurrencyExchange/src/currencies/rate/Rate.java
+++ CurrencyExchange/src/currencies/rate/Rate.java
@@ -33,6 +33,34 @@ public class Rate {
 	public void setPurchasable(double purchasable) {
 		this.purchasable = purchasable;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rate other = (Rate) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Rate [Date=" + date + ", Sales=" + sales + ", Medium=" + medium + ", Purchasable=" + purchasable + "]";
+	}
+	
 	
 	
 }
